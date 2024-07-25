3ef3315c0ac5278748459f2c14e607b8f28205af
mattmc3 <mattmc3@gmail.com>
2024-07-24 21:25:17 -0400

    Update fish completions

--- completions/otp.fish
+++ completions/otp.fish
@@ -1,12 +1,3 @@
-function __otp_show_keys
-    # Tokenize the current process, up to the cursor.
-    # -o means tokenize, -p means current process only, -c means stop at cursor.
-    set tokens (commandline -opc)
-    test (count $tokens) -eq 1
-    or contains -- '-r' $tokens
-    or contains -- '--remove' $tokens
-end
-
 complete -c otp --no-files --exclusive -s "l" -l "list" -d "List available OTP keys"
 complete -c otp --no-files --exclusive -s "h" -l "help" -d "Display OTP help"
 complete -c otp --no-files --exclusive -s "a" -l "add" -d "Add OTP key"
